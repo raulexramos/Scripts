5284d5fbaddab122b301e9956609aba4cd8dafb7
Raulex Ramos <86503819+raulexramos@users.noreply.github.com>
2021-09-07 20:09:17 -0300

    Verifica nulnerabilidades em site
    Necessário ter o Nmap instalado e ter # privilégios no terminal

--- vulnerabilidadeSite.sh
+++ vulnerabilidadeSite.sh
@@ -10,14 +10,17 @@
 echo "############################"
 echo "# PESQUISA VULNERABILIDADE #"
 echo "############################"
-echo "Digite um dominio: "
+echo "Digite o domínio abaixo: " 
+echo "(ex. site.com.br)" 
 read userDominio
 sudo nmap -sS -sC -Pn --script vuln $userDominio
 
-echo "#######################################################"
-echo "Agora iremos verificar se tem exploits para este site: " $userDominio
-sudo nmap -Pn -sS -sC --script exploit $userDminio
-
 echo "##########################################################"
-echo "Opa, agora vamos ver se está vulnerável a ataques DoS \o/ "
+echo "Agora iremos verificar se tem exploits para este site: " $userDominio
+echo "#########################################################"
+sudo nmap -Pn -sS -sC --script exploit $userDominio
+
+echo "#########################################################"
+echo "Opa, agora vamos ver se está vulnerável a ataques DoS \o/ " $userDominio
+echo "#########################################################"
 sudo nmap -Pn -sS -sC --script dos $userDominio
